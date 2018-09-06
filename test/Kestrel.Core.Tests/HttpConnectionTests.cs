d8934d1275662481650f014202d399a23ae7ced2
Stephen Halter <halter73@gmail.com>
2018-09-06 15:41:14 -0700

    Add TimeoutControl class

--- test/Kestrel.Core.Tests/HttpConnectionTests.cs
+++ test/Kestrel.Core.Tests/HttpConnectionTests.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+#if false
+
 using System;
 using System.Buffers;
 using System.Collections.Generic;
@@ -589,3 +591,5 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Tests
         }
     }
 }
+
+#endif
